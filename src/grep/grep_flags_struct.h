737ea37a4a5dbfd8c188fe68ee3a995dd7660907
fnch1q <rmr@labis.su>
2024-06-12 17:44:25 +0900

    flag -c

--- src/grep/grep_flags_struct.h
+++ src/grep/grep_flags_struct.h
@@ -5,6 +5,7 @@
 typedef struct {
     bool is_pattern;          // флаг шаблона
     bool is_ignore_register;  // нумерует непустые строки
+    bool is_count_rows;       // выводит количество строк
 } Grep_flags;
 
 #endif
--- src/grep/grep_functions.c
+++ src/grep/grep_functions.c
@@ -3,6 +3,7 @@
 void output(char *files[], Grep_flags *flags, int file_count, char *pattern) {
     char buf[1000];
     char buf_output[1000];
+    int count_rows;
 
     for (int i = 0; i < file_count; i++) {
         FILE *f = fopen(files[i], "r");
@@ -10,6 +11,8 @@ void output(char *files[], Grep_flags *flags, int file_count, char *pattern) {
             printf("%s: Нет такого файла или каталога\n", files[i]);
             continue;
         }
+
+        count_rows = 0;
 
         while (fgets(buf, 1000, f)) {
             strcpy(buf_output, buf);
@@ -22,11 +25,20 @@ void output(char *files[], Grep_flags *flags, int file_count, char *pattern) {
             if (!strstr(buf, pattern)) {
                 continue;
             }
-            printf("%s", buf_output);
+
+            if (flags->is_count_rows) {
+                count_rows++;
+            } else {
+                printf("%s", buf_output);
+            }
         }
+
+        if (flags->is_count_rows) {
+            printf("%d", count_rows);
+        }
+
         fclose(f);
     }
-    printf("\n");
 }
 
 char *str_lower(char *string) {

ab0d5da0fa23358bd3afc4a2db6fa34862ff72de
StanislavLabis <psa@labis.su>
2024-06-12 15:24:35 +0900

    Revert "add grep files, makefile upd"
    This reverts commit 085396e387f08d8ab5401d4c45d7762dbe5f58a4.

--- src/grep/parsing_grep.h
+++
@@ -1,8 +0,0 @@
-#include "grep_functions.h"
-
-#ifndef PARSING_GREP_H
-#define PARSING_GREP_H
-
-char *parse_args(int argc, char *argv[], Grep_flags *flags, char *files[], int *file_count, char **pattern);
-
-#endif

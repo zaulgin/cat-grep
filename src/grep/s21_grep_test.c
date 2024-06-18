f86c883a0f29b9487229a76eb98f9991aabfab39
StanislavLabis <psa@labis.su>
2024-06-19 08:21:13 +0900

    тест доделать

--- src/grep/s21_grep_test.c
+++ src/grep/s21_grep_test.c
@@ -3,11 +3,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *add_log(char string[], char log_message[]) {
+  char *log = malloc(1000);
+  strcat(log, string);
+  strcat(log, log_message);
+  return log;
+}
+
+char *add_file(char string[], char filename[]) {
+  char *file = malloc(1000);
+  strcat(file, string);
+  strcat(file, " ");
+  strcat(file, filename);
+  return file;
+}
+
+char *add_pattern(char string[], char pattern_name[]) {
+  char *pattern = malloc(1000);
+  strcat(pattern, string);
+  strcat(pattern, " -e ");
+  strcat(pattern, pattern_name);
+  return pattern;
+}
+
 int main() {
-  // char patterns[] = "src_test/patterns.txt";
   char files[][26] = {"src_test/english_test.txt", "src_test/russian_test.txt",
                       "src_test/symbols_test.txt"};
-  // char minus[] = "-";
+  char patterns[][20] = {"ам",
+                         "АМЕРИКИ",
+                         "are",
+                         "WiNdoW",
+                         "Согл",
+                         "[0-9]",
+                         "[d-e]",
+                         "[а-б]"
+                         "1",
+                         "@"};
   char options[][2] = {"i",
                        "v",
                        "s",
@@ -17,43 +48,58 @@ int main() {
                        "h",
                        "o"};
   // char options_for_patterns[][2] = {"e", "f"};
-  char grep[] = "grep";
-  char s21_grep[] = "./s21_grep";
-  bool is_single_pattern = false;
+  char grep[] = "grep -";
+  char s21_grep[] = "./s21_grep -";
 
   char out_grep[1000];
   char out_s21_grep[1000];
+  strcpy(out_grep, grep);
+  strcpy(out_s21_grep, s21_grep);
+  char with_files_grep[1000];
+  char with_files_s21_grep[1000];
 
+  char with_pattern_grep[1000];
+  char with_pattern_s21_grep[1000];
+
+  char *output_grep = NULL;
+  char *output_s21_grep = NULL;
+  // выделить память для output
   for (int i = 0; i < 8; i++) {
-    strcpy(out_grep, grep);
-    strcpy(out_s21_grep, s21_grep);
-
-    strcat(out_grep, "-");
-    strcat(out_s21_grep, "-");
-
     strcat(out_grep, options[i]);
     strcat(out_s21_grep, options[i]);
-    if (!is_single_pattern) {
-      strcat(out_grep, " are ");
-      strcat(out_s21_grep, " are ");
-      is_single_pattern = true;
+    // printf("%s\n", out_grep);
+    // printf("%s\n", out_s21_grep);
+    strcpy(with_pattern_grep, "");
+    strcpy(with_pattern_s21_grep, "");
+    for (int k = 0; k < 10; k++) {
+      strcat(with_pattern_grep, " -e ");
+      strcat(with_pattern_s21_grep, " -e ");
+      strcat(with_pattern_grep, patterns[k]);
+      strcat(with_pattern_s21_grep, patterns[k]);
+      for (int j = 0; j < 3; j++) {
+        strcat(with_files_grep, " ");
+        strcat(with_files_s21_grep, " ");
+
+        sprintf(output_grep, "%s%s%s >> log_grep.log", out_grep,
+                with_pattern_grep, with_files_grep);
+        sprintf(output_s21_grep, "%s%s%s >> log_s21_grep.log", out_s21_grep,
+                with_pattern_s21_grep, with_files_s21_grep);
+        system(output_grep);
+        system(output_s21_grep);
+        strcpy(output_grep, "");
+        strcpy(output_s21_grep, "");
+      }
     }
-    for (int j = 0; j < 3; j++) {
-      strcat(out_grep, files[j]);
-      strcat(out_s21_grep, files[j]);
-      printf("%s >> log_grep.txt\n", out_grep);
-      printf("%s >> log_s21_grep.txt", out_s21_grep);
-    }
+
+    char res[2000];
+    system("diff log_grep.log log_s21_grep.log > changes.diff");
+    FILE *g = fopen("changes.diff", "r");
+    if (fgets(res, sizeof(res), g)) {
+      printf("TEST FAILED\n");
+    } else
+      printf("TEST SUCCESS\n");
+
+    fclose(g);
+    return 0;
   }
-
-  char res[2000];
-  system("diff log_grep.txt log_s21_grep.txt > changes.diff");
-  FILE *g = fopen("changes.diff", "r");
-  if (fgets(res, sizeof(res), g)) {
-    printf("TEST FAILED\n");
-  } else
-    printf("TEST SUCCESS\n");
-
-  fclose(g);
-  return 0;
 }
